6e371315e9800eccd9f8cec2eac1cac5269fd1b8
gillancodes <gillancodes@laposte.net>
2022-05-23 17:26:30 +0200

    Added : CRTL +D to delete a line && Fix : preventDefault on Enter hit

--- editor/index.ts
+++ editor/index.ts
@@ -8,7 +8,7 @@ function setCaret(lineId) {
     var sel = window.getSelection()
 
     console.log(el.childNodes)
-    
+    console.log(el.innerText.length)
     range.setStart(el.childNodes[0], 0)
     range.collapse(true)
     
@@ -16,8 +16,9 @@ function setCaret(lineId) {
     sel.addRange(range)
 }
 
-const keyMap = {enter: 13, backSpace:8, arrowUp: 38, arrowDown: 40};
+const keyMap = {enter: 13, backSpace:8, arrowUp: 38, arrowDown: 40, ctrl: 17};
 var dLnTry = 0;
+var lastInput;
 
 class gcEditor {
     element: Element
@@ -50,42 +51,59 @@ class gcEditor {
             ln.id = uid();
             ln.className = "line";
             ln.contentEditable = "true";
-            ln.innerHTML = "New Content"
+            ln.innerText = "\n"
             this.element.appendChild(ln);
             setCaret(ln.id)
         return ln
     }
 
-    deleteLine(lineId) {
-        var ln = document.getElementById(lineId);
-        if (ln.innerText === '\n'){
-            dLnTry = dLnTry+1
-            if (dLnTry == 2) {
-                ln.remove();
-            }
-        } else {
-            dLnTry = 0;
-        }
-        console.log(dLnTry)
+    deleteLine(ln) {
+
+        setCaret(ln.previousElementSibling.id)
+        ln.remove();
+        
     }
 
     onkeyup(event) {
+        event.preventDefault();
         console.log(event.keyCode, "-> Key =>", event.key)
 
         if (event.keyCode != keyMap.backSpace) {
             dLnTry = 0
         }
 
+
+        console.log(lastInput, event.keyCode)
+        //ctrl + d
+        if (lastInput == keyMap.ctrl && event.keyCode == 68) {
+           this.deleteLine(document.getElementById(event.target.id))
+        }
+
         switch(event.keyCode) {
             case keyMap.enter:
+                event.preventDefault();
                 this.createLine();
                 break;
             case keyMap.backSpace:
-                this.deleteLine(event.target.id)
+                var ln = document.getElementById(event.target.id);
+                if (ln.innerText === '\n'){
+                    dLnTry = dLnTry+1
+                    if (dLnTry == 2) {
+                        if (ln.previousElementSibling) {
+                            this.deleteLine(ln)
+                        }
+                        dLnTry = 0              
+                    }
+                } else {
+                    dLnTry = 0;
+                }
+                // this.deleteLine(event.target.id)
                 break
             default:
                 break
         }
+
+        lastInput = event.keyCode;
     }
 
     getContent() {
