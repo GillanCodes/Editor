70142f26cb2ad81009e0f6788b26a7e5b9fc2f56
gillancodes <gillancodes@laposte.net>
2022-05-24 15:04:10 +0200

    Added : Navigation With arrowUp & Arrow Down, event keyUp replace by KeyDown (many fixs qith that), Menu section that allow you to add heading (titler), paragraph and image

--- editor/index.ts
+++ editor/index.ts
@@ -6,28 +6,31 @@ function setCaret(lineId) {
     var el = document.getElementById(lineId)
     var range = document.createRange()
     var sel = window.getSelection()
-
-    console.log(el.childNodes)
-    console.log(el.innerText.length)
-    range.setStart(el.childNodes[0], 0)
+    range.setStart(el.lastChild, 0)
     range.collapse(true)
     
     sel.removeAllRanges()
     sel.addRange(range)
 }
 
-const keyMap = {enter: 13, backSpace:8, arrowUp: 38, arrowDown: 40, ctrl: 17};
+const keyMap = {enter: 13, backSpace:8, arrowUp: 38, arrowDown: 40, ctrl: 17, tab: 9};
 var dLnTry = 0;
 var lastInput;
+var lastSelect;
 
 class gcEditor {
-    element: Element
+    element: HTMLElement
     content: string[]
+    menu: HTMLElement;
 
-    constructor ({editor, content}) {
+    constructor ({editor, menu, content}) {
         this.element = document.getElementById(editor),
+        this.menu = document.getElementById(menu)
         this.content = content;
         this.onkeyup = this.onkeyup.bind(this);
+        this.onKeyDown = this.onKeyDown.bind(this);
+        this.onClick = this.onClick.bind(this);
+        this.menuClickHandle = this.menuClickHandle.bind(this);
     }
 
     init() {
@@ -38,76 +41,164 @@ class gcEditor {
             throw Error("'editor' params must be an ID of a 'Div' element !")
     
         if(this.content == undefined) {
-            this.createLine();
+            this.createLine(this.element.firstChild);
         }
 
-        document.addEventListener('keyup', this.onkeyup)
+        document.addEventListener('keyup', this.onkeyup);
+        document.addEventListener('click', this.onClick);
+        document.addEventListener('keydown', this.onKeyDown);
         
+
+
+        if (this.menu == null)
+            throw Error("'menu' params do not exist")
+        if (this.menu.tagName !== "DIV")
+            throw Error("'menu' params must be an ID of a 'Div' element !")
+
+        var menuEle = document.createElement("div")
+        menuEle.className = "menu"
+        menuEle.innerHTML = "<ul><li id='title'>Heading</li><li id='paragraph'>Normal</li><li id='picture'>Pic</li></ul><ul><li>button</li></ul>";
+        menuEle.addEventListener('click', this.menuClickHandle)
+        this.menu.replaceWith(menuEle)
 
     }
 
-    createLine() {
+    createLine(child) {
+        
         var ln = document.createElement('p');
             ln.id = uid();
             ln.className = "line";
             ln.contentEditable = "true";
             ln.innerText = "\n"
-            this.element.appendChild(ln);
+            this.element.insertBefore(ln, child);
             setCaret(ln.id)
         return ln
     }
 
-    deleteLine(ln) {
-
-        setCaret(ln.previousElementSibling.id)
-        ln.remove();
         
+
+
+    deleteLine(ln) {
+        try {
+            setCaret(ln.previousElementSibling.id)
+        } catch (error) {
+            setCaret(this.element.firstElementChild.id)
+        }
+        ln.remove();
     }
 
-    onkeyup(event) {
-        event.preventDefault();
-        console.log(event.keyCode, "-> Key =>", event.key)
-
-        if (event.keyCode != keyMap.backSpace) {
-            dLnTry = 0
+    onClick(event) {
+        //setCaret(event.target)
+        if (event.target.id === this.element.id) {
+            setCaret(event.target.childNodes[1].id);
         }
 
-
-        console.log(lastInput, event.keyCode)
-        //ctrl + d
-        if (lastInput == keyMap.ctrl && event.keyCode == 68) {
-           this.deleteLine(document.getElementById(event.target.id))
+        if (event.target.className === "line") {
+            lastSelect = event.target.id;
         }
+    }
+
+    onKeyDown (event) {
 
         switch(event.keyCode) {
+            case keyMap.tab:
+                event.preventDefault();
+                break;
             case keyMap.enter:
                 event.preventDefault();
-                this.createLine();
+                this.createLine(event.target.nextElementSibling);
+                break;
+            case keyMap.arrowUp:
+                event.preventDefault();
+                if (event.target.previousElementSibling){
+                    setCaret(event.target.previousElementSibling.id);
+                }
+                break;
+            case keyMap.arrowDown:
+                event.preventDefault();
+                if (event.target.nextElementSibling){
+                    setCaret(event.target.nextElementSibling.id);
+                }
                 break;
             case keyMap.backSpace:
                 var ln = document.getElementById(event.target.id);
-                if (ln.innerText === '\n'){
-                    dLnTry = dLnTry+1
-                    if (dLnTry == 2) {
+                if (ln === null) {
+                    if (lastSelect) {
+                        var notALine = document.getElementById(lastSelect)
+                        if (notALine.tagName === "IMG") {
+                            if (notALine.previousElementSibling) {
+                                this.deleteLine(notALine);
+                            } else {
+                                this.createLine(notALine);
+                                this.deleteLine(notALine)
+                            }
+                        }
+                        
+                    }
+                } else {
+                    if (ln.innerText === ''){
                         if (ln.previousElementSibling) {
                             this.deleteLine(ln)
                         }
-                        dLnTry = 0              
                     }
-                } else {
-                    dLnTry = 0;
                 }
-                // this.deleteLine(event.target.id)
                 break
             default:
                 break
         }
 
-        lastInput = event.keyCode;
+    }
+
+    menuClickHandle(event) {
+        switch(event.target.id){
+            case 'title':
+                var selected = document.getElementById(lastSelect);
+                var heading =  document.createElement('h1');
+                heading.className = "line";
+                heading.id = selected.id;
+                heading.innerHTML = selected.innerHTML;
+                heading.contentEditable = "true"
+                selected.replaceWith(heading)
+                break;
+            case 'paragraph':
+                var selected = document.getElementById(lastSelect);
+                var paragraph =  document.createElement('p');
+                paragraph.className = "line";
+                paragraph.id = selected.id;
+                paragraph.innerHTML = selected.innerHTML;
+                paragraph.contentEditable = "true"
+                selected.replaceWith(paragraph)
+                break;
+            case 'picture':
+                var selected = document.getElementById(lastSelect);
+                var input =  document.createElement('input');
+                input.className = "line";
+                input.id = selected.id;
+                input.type = "file"
+                input.addEventListener('change', (event) => {
+                    var file = (<HTMLInputElement>event.target).files[0];
+                    var blob = URL.createObjectURL(file);
+                    var img = document.createElement('img');
+                    img.className = "line";
+                    img.id = selected.id;
+                    img.src = blob;
+                    img.contentEditable = "true"
+                    input.replaceWith(img)
+                    
+                })
+                selected.replaceWith(input)
+                break;
+            default:
+                break;
+        }
+    } 
+
+    onkeyup(event) {
+        
     }
 
     getContent() {
-        return this.element.childNodes;
+        return this.element.children;
     }
 
     setContent(content) {
